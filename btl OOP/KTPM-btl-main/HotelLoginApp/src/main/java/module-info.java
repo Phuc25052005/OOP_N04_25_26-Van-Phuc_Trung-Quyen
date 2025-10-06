7a6647976a56008b7b2915cd2fa1d3ee73e85f3e
TrungQuyen00 <151829269+TrungQuyen00@users.noreply.github.com>
2025-10-06 16:22:37 +0700

    Update module-info.java

--- btl OOP/KTPM-btl-main/HotelLoginApp/src/main/java/module-info.java
+++ btl OOP/KTPM-btl-main/HotelLoginApp/src/main/java/module-info.java
@@ -1,21 +1,14 @@
 module com.example.hotelloginapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.web;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.context;
+    requires spring.web;
+    requires spring.data.jpa;
+    requires jakarta.persistence;
     requires java.sql;
-    requires java.desktop;
-    opens com.example.hotelloginapp.models to javafx.base;
-    opens com.example.hotelloginapp.controller to javafx.fxml;
-    opens com.example.hotelloginapp to javafx.graphics, javafx.fxml;
-//    exports com.example.hotellogin;
+
+    opens com.example.hotelloginapp.model to spring.core, spring.beans, spring.context, jakarta.persistence;
+    opens com.example.hotelloginapp.controller to spring.core, spring.beans, spring.web;
     exports com.example.hotelloginapp;
     exports com.example.hotelloginapp.controller;
 }
